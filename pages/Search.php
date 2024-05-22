ab447bd3c279e52cd7b3eee301432f037e6e17e3
volodymyrkorobkin <6040683@mborijnland.nl>
2024-05-22 11:06:30 +0200

    fix paths

--- pages/Search.php
+++ pages/Search.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
     <title>Document</title>
     <?php include './scripts/fetch.php' ?>
 </head>
@@ -11,7 +11,7 @@
 
 <nav class="navbar-2">
     <ul class="nav-links-2">
-    <img src="img/HOBO_logo.png"  alt="">
+    <img src="/img/HOBO_logo.png"  alt="">
     </ul>
 
 
@@ -19,13 +19,13 @@
     <nav class="navbar">
         
         <ul class="nav-links">
-            <li class="nav-items"><a href="Search.php">  <img src="img/free-search-icon-2911-thumb.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="Search.php">  <img src="/img/free-search-icon-2911-thumb.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="index.php">  <img src="img/Home_icon_blue-1.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="index.php">  <img src="/img/Home_icon_blue-1.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="Account.php">  <img src="img/profile-icon-9.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="Account.php">  <img src="/img/profile-icon-9.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="Settings.php">  <img src="img/purepng.com-settings-icon-android-kitkatsymbolsiconsapp-iconsandroid-kitkatandroid-44-721522597677p9lc8.png"  alt=""></a></li>
+            <li class="nav-items"><a href="Settings.php">  <img src="/img/purepng.com-settings-icon-android-kitkatsymbolsiconsapp-iconsandroid-kitkatandroid-44-721522597677p9lc8.png"  alt=""></a></li>
         </ul>
     </nav>
     <div class="search">
--- pages/home.php
+++ pages/home.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
     <title>Document</title>
-    <?php include './scripts/fetch.php' ?>
 </head>
 <body>
 
 <nav class="navbar-2">
     <ul class="nav-links-2">
-    <img src="img/HOBO_logo.png"  alt="">
+    <img src="/img/HOBO_logo.png"  alt="">
     </ul>
 
     <div class="right-container">
@@ -23,13 +22,13 @@
     <nav class="navbar">
         
         <ul class="nav-links">
-            <li class="nav-items"><a href="Search.php">  <img src="img/free-search-icon-2911-thumb.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="Search.php">  <img src="/img/free-search-icon-2911-thumb.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="index.php">  <img src="img/Home_icon_blue-1.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="index.php">  <img src="/img/Home_icon_blue-1.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="Account.php">  <img src="img/profile-icon-9.png"  alt=""></a></li></ul>
+            <li class="nav-items"><a href="Account.php">  <img src="/img/profile-icon-9.png"  alt=""></a></li></ul>
             <ul class="nav-links">
-            <li class="nav-items"><a href="Settings.php">  <img src="img/purepng.com-settings-icon-android-kitkatsymbolsiconsapp-iconsandroid-kitkatandroid-44-721522597677p9lc8.png"  alt=""></a></li>
+            <li class="nav-items"><a href="Settings.php">  <img src="/img/purepng.com-settings-icon-android-kitkatsymbolsiconsapp-iconsandroid-kitkatandroid-44-721522597677p9lc8.png"  alt=""></a></li>
         </ul>
     </nav>
    
@@ -43,66 +42,66 @@
                 <h1 class="movie-title">Test</h1>
                 <p class="movie-des">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit saepe eius ratione nostrum mollitia explicabo quae nam pariatur. Sint, odit?</p>
             </div>
-            <img src="img/seriesCards/00602.jpg" alt="">
+            <img src="/img/seriesCards/00602.jpg" alt="">
         </div>
         
     </div>
 </div>
     <div class="movies-list">
-        <button class="pre-btn"><img src="img/pre.png" alt=""></button>
-        <button class="nxt-btn"><img src="img/nxt.png" alt=""></button>
+        <button class="pre-btn"><img src="/img/pre.png" alt=""></button>
+        <button class="nxt-btn"><img src="/img/nxt.png" alt=""></button>
         <div class="card-container">
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00002.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00002.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00003.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00003.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
@@ -112,60 +111,60 @@
             
         </div>
         <div class="movies-list">
-        <button class="pre-btn"><img src="img/pre.png" alt=""></button>
-        <button class="nxt-btn"><img src="img/nxt.png" alt=""></button>
+        <button class="pre-btn"><img src="/img/pre.png" alt=""></button>
+        <button class="nxt-btn"><img src="/img/nxt.png" alt=""></button>
         <div class="card-container">
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00002.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00002.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00003.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00003.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
@@ -175,60 +174,60 @@
             
         </div>
         <div class="movies-list">
-        <button class="pre-btn"><img src="img/pre.png" alt=""></button>
-        <button class="nxt-btn"><img src="img/nxt.png" alt=""></button>
+        <button class="pre-btn"><img src="/img/pre.png" alt=""></button>
+        <button class="nxt-btn"><img src="/img/nxt.png" alt=""></button>
         <div class="card-container">
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00002.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00002.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00003.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00003.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
@@ -238,60 +237,60 @@
             
         </div>
         <div class="movies-list">
-        <button class="pre-btn"><img src="img/pre.png" alt=""></button>
-        <button class="nxt-btn"><img src="img/nxt.png" alt=""></button>
+        <button class="pre-btn"><img src="/img/pre.png" alt=""></button>
+        <button class="nxt-btn"><img src="/img/nxt.png" alt=""></button>
         <div class="card-container">
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00002.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00002.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00003.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00003.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
                 </div>
             </div> 
             <div class="card">
-                <img src="img/seriesCards/00001.jpg" class="card-img" alt="">
+                <img src="/img/seriesCards/00001.jpg" class="card-img" alt="">
                 <div class="card-body">
                     <h2 class="name">movie name</h2>
                     <h6 class="des">Lorem ipsum dolor sit amet consectetur.</h6>
