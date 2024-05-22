9adca523f4486123d1da085130743068e064453c
volodymyrkorobkin <6040683@mborijnland.nl>
2024-05-22 11:27:38 +0200

    Add sql connect/utils

---
+++ php/sql_utils.php
@@ -0,0 +1,24 @@
+<?php
+
+function runSql($sql, $params = []) {
+    global $conn;
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($params);
+    return $stmt;
+}
+
+function runSqlreturnConn($sql, $params = []) {
+    global $conn;
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($params);
+    return $conn;
+}
+
+
+function fetchSql($sql, $params = []) {
+    return runSql($sql, $params)->fetch(PDO::FETCH_ASSOC);
+}
+
+function fetchSqlAll($sql, $params = []) {
+    return runSql($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+}
